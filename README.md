d9ee64c289683651283432ba46de468a0bcce3d9
Jiangjiajun-Dominic <710120955@qq.com>
2021-03-10 00:07:59 -0800

    [Description] create pause and resume slots [Developer] Jiajun Jiang [Commit] 16:05 10/Mar

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# main
+# Smart-intonation-test
+
+A simple example program using Qt/Qwt widgets.
+
+
+
+## Making it work
+
+Install the QT5 and Qwt development packages:
+
+```
+    apt-get install qtdeclarative5-dev-tools
+    apt-get install libqwt-qt5-dev
+```
+
+To clone this git repository.
+
+To build:
+
+```
+    cd build
+    cmake ..
+    make
+```
+
+To run:
+
+```
+    ./qwt-example
+```
+
---
+++ window.cpp
@@ -0,0 +1,249 @@
+#include "window.h"
+ //#include "adcreader.h"
+#include <QAudioInput>
+#include <QAudioRecorder>
+#include <QMediaPlayer>
+//#include <QVideoWidget>
+#include <cmath>  // for sine stuff
+
+
+Window::Window() : gain(5), count(0)
+{
+	setWindowTitle("Welcome to the Smart Intonation Game");  //set title
+    //knob = new QwtKnob;
+	// set up the gain knob
+	//knob->setValue(gain);
+
+	// use the Qt signals/slots framework to update the gain -
+	// every time the knob is moved, the setGain function will be called
+	//connect( knob, SIGNAL(valueChanged(double)), SLOT(setGain(double)) );
+    ///// Audio Recorder //////
+	
+
+	//set push buttons
+	pushbutton1 = new QPushButton;            
+	pushbutton1->setText(tr("Learning"));         //type on the button
+	pushbutton1->setFixedHeight(40);             //set a fixed height
+	pushbutton1->setFixedWidth(150);            //set a fixed width
+
+	pushbutton2 = new QPushButton;
+	pushbutton2->setText(tr("Testing"));
+	pushbutton2->setFixedHeight(40);
+	pushbutton2->setFixedWidth(150);
+
+    pushbutton3 = new QPushButton;
+	pushbutton3->setText(tr("Recognising"));
+	pushbutton3->setFixedHeight(40);
+	pushbutton3->setFixedWidth(150);
+
+	quitbutton = new QPushButton;
+	quitbutton->setText(tr("Quit"));
+	quitbutton->setFixedHeight(30);
+	quitbutton->setFixedWidth(100);
+
+    stopbutton = new QPushButton;
+	stopbutton->setText(tr("stop"));
+	stopbutton->setFixedHeight(30);
+	stopbutton->setFixedWidth(100);
+
+    pausebutton = new QPushButton;
+	pausebutton->setText(tr("pause"));
+	pausebutton->setFixedHeight(30);
+	pausebutton->setFixedWidth(100);
+
+    resumebutton = new QPushButton;
+	resumebutton->setText(tr("resume"));
+	resumebutton->setFixedHeight(30);
+	resumebutton->setFixedWidth(100);
+	//set pushbuttons colors
+	//setStyleSheet("QPushButton::checked{background-color: red;color: white;} "
+    //  "\n "
+    // "QPushButton{background-color:  black;color: white;}");
+					   
+    //QPushButton *pushbutton1 = new QPushButton(this);      // to open new window when pressing pushbutton
+    // QWidget *widget = new QWidget(this);
+    //widget->setWindowFlags(Qt::Window);
+     // widget->setWindowTitle("New Window");
+   //connect(pushbutton1, SIGNAL(clicked()), widget, SLOT(show()));
+
+   connect(pushbutton1, SIGNAL(clicked()), this, SLOT(playslot()));
+   connect(stopbutton, SIGNAL(clicked()), this, SLOT(stopslot()));
+   //connect(controls, SIGNAL(stop()), player, SLOT(stop()));
+   connect(quitbutton, SIGNAL(clicked()), this, SLOT(quitApp()));
+   connect(pausebutton, SIGNAL(clicked()), this, SLOT(pauseslot()));
+   connect(resumebutton, SIGNAL(clicked()), this, SLOT(resumeslot()));
+   
+	pushbutton1->show();
+	pushbutton2->show();
+    pushbutton3->show();
+	quitbutton->show();
+
+
+	//audioRecorder = new QAudioRecorder;
+   // QAudioEncoderSettings audioSettings;
+   // audioSettings.setCodec("audio/amr");
+   // audioSettings.setQuality(QMultimedia::HighQuality);
+  // audioRecorder->setEncodingSettings(audioSettings);
+   // audioRecorder->setOutputLocation(QUrl::fromLocalFile("home/mario/project2"));
+   // audioRecorder->record();
+    ///
+	
+	// set up the initial plot data
+	//for( int index=0; index<plotDataSize; ++index )
+	//{
+		//xData[index] = index;
+		//yData[index] = gain * sin( M_PI * index/50 );
+	//}
+
+	//curve = new QwtPlotCurve;
+	
+	// make a plot curve from the data and attach it to the plot
+	//curve->setSamples(xData, yData, plotDataSize);
+	//curve->attach(plot);
+
+	//plot->replot();
+//	plot->show();
+       
+	// set up the layout - knob above thermometer
+	vLayout = new QVBoxLayout;
+	//vLayout->addWidget(counter);
+	//vLayout->addWidget(clock);
+	//vLayout->addWidget(knob);
+	//vLayout->addWidget(thermo);
+	//vLayout->addWidget(w);
+	
+	vLayout->addWidget(pushbutton1); 
+	  
+	vLayout->addWidget(pushbutton2);
+	vLayout->addWidget(pushbutton3);
+	
+    vLayout->addWidget(quitbutton);
+	
+	// plot to the left of knob and thermometer
+	hLayout = new QHBoxLayout;
+	hLayout->addLayout(vLayout);
+	//hLayout->addWidget(w);
+   
+	
+	
+	//hLayout->addWidget(plot);           ///plot the graph on the same interface
+    
+	setLayout(hLayout);
+
+	// This is a demo for a thread which can be
+	// used to read from the ADC asynchronously.
+	// At the moment it doesn't do anything else than
+	// running in an endless loop and which prints out "tick"
+	// every second.
+//	adcreader = new ADCreader();
+//	adcreader->start();
+}
+
+Window::~Window() {
+	// tells the thread to no longer run its endless loop
+//	adcreader->quit();
+	// wait until the run method has terminated
+//	adcreader->wait();
+//	delete adcreader;
+}
+
+void Window::timerEvent( QTimerEvent * )
+{
+	//double inVal = gain * sin( M_PI * count/50.0 );
+	//++count;
+
+	// add the new input to the plot
+	//memmove( yData, yData+1, (plotDataSize-1) * sizeof(double) );
+	//yData[plotDataSize-1] = inVal;
+  
+	//curve->setSamples(xData, yData, plotDataSize);
+	//plot->replot();
+
+	// set the thermometer value
+	//thermo->setValue( inVal + 10 );
+}
+
+
+// this function can be used to change the gain of the A/D internal amplifier
+void Window::setGain(double gain)
+{
+	// for example purposes just change the amplitude of the generated input
+	this->gain = gain;
+}
+
+//this function is used to act whenever pushbutton 1 is pressed
+void Window::playslot()    
+{  
+	 
+	 vLayout->addWidget(pausebutton);
+	 vLayout->addWidget(resumebutton);
+	 vLayout->addWidget(stopbutton); 
+	 vLayout->addWidget(quitbutton); 
+	 
+	 player = new QMediaPlayer;
+	 player->setMedia(QUrl::fromLocalFile("/home/jiangjiajun/Testing0/C-scale.mp3"));
+	 player->setVolume(50);           // set the volume 
+     player->play();                 //play the song
+	 pushbutton1->hide();     //hide every button in the app while listening to avoid conflicts
+	 pushbutton2->hide();
+	 pushbutton3->hide();
+	pausebutton->show();     
+	resumebutton->show();
+	stopbutton->show();
+
+
+} 
+//this function is used to stop the learning video then back to the first window to enable the user to press any button
+void Window::stopslot()  
+{ 
+    player->stop();             //stop the song  
+    pushbutton1->show();     
+	pushbutton2->show();
+	pushbutton3->show();
+	pushbutton1->setEnabled(true);
+    pushbutton2->setEnabled(true);
+	pushbutton3->setEnabled(true);
+    pausebutton->hide();     //hide every button in the app while listening to avoid conflicts
+	resumebutton->hide();
+	stopbutton->hide();
+
+
+}
+//this function is used to pause the video
+void Window::pauseslot()    
+{  
+	 
+	 vLayout->addWidget(pausebutton);
+	 vLayout->addWidget(resumebutton);
+	 vLayout->addWidget(stopbutton); 
+	 vLayout->addWidget(quitbutton); 
+
+     player->pause();                 //pause the song
+	 pushbutton1->hide();     //hide every button in the app while listening to avoid conflicts
+	 pushbutton2->hide();
+	 pushbutton3->hide();
+	 
+
+} 
+
+void Window::resumeslot()    
+{  
+
+	 vLayout->addWidget(pausebutton);
+	 vLayout->addWidget(resumebutton);
+	 vLayout->addWidget(stopbutton); 
+	 vLayout->addWidget(quitbutton); 
+	 
+     player->play();                 //resume the song
+	 pushbutton1->hide();     //hide every button in the app while listening to avoid conflicts
+	 pushbutton2->hide();
+	 pushbutton3->hide();
+} 
+//this function is used to quit the application
+void Window::quitApp()    
+{ 
+    Window::close();
+   
+}
+
+
